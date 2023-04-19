ece6a1f7aa91190f4d47ccea4ba14d23756ef099
awais97 <awaishassan1997@gmail.com>
2023-04-19 16:02:43 +0500

    Backend setup with openi and cloudinary in progress

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router;
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openapi = new OpenAIApi(config)
